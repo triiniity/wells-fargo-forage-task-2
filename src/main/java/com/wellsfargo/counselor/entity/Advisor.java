29da93553785963ed7a3e1dfe2b58b554d538fb9
tri_nity <202001430@daiict.ac.in>
2023-08-31 05:01:36 +0530

    Commented Adivsor.java
    added comments for better understanding

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -6,13 +6,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+// This is a Java class called `Advisor` that represents an entity in a database. 
 @Entity
 public class Advisor {
-
+    
+    // The `@Id` annotation is used to specify that the `advisorId` field is the primary key of the table and should be generated automatically.
     @Id
     @GeneratedValue()
     private long advisorId;
-
+    
+    // The class has six instance variables, which are annotated with `@Column` to specify that they should be mapped to columns in the database table. 
+    
     @Column(nullable = false)
     private String firstName;
 
@@ -32,6 +36,10 @@ public class Advisor {
 
     }
 
+    // The class has two constructors, one of which is protected and the other is public. 
+    // The public constructor takes five parameters, which are used to initialize the instance variables. 
+    // The protected constructor is empty and is used by the JPA provider to create instances of the class. (see above)
+
     public Advisor(String firstName, String lastName, String address, String phone, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -39,6 +47,9 @@ public class Advisor {
         this.phone = phone;
         this.email = email;
     }
+
+    // The class has getter and setter methods for each of the instance variables, which are used to access and modify the values of the fields. 
+    // The getter methods return the values of the fields, while the setter methods set the values of the fields to the specified values.
 
     public Long getAdvisorId() {
         return advisorId;
@@ -84,3 +95,5 @@ public class Advisor {
         this.email = email;
     }
 }
+
+// Overall, this class represents an entity in a database table and provides methods to access and modify the values of the fields.
